673f7d103d759999eef0f4d9af80f1a0b60c777c
Moritz Ma <st187710@stud.uni-stuttgart.de>
2024-01-19 12:41:21 +0100

    added JavaDoc

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet11/homework/MemoryHamsterGame.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet11/homework/MemoryHamsterGame.java
@@ -27,16 +27,15 @@ public class MemoryHamsterGame extends SimpleHamsterGame {
     @Override
     protected void run() {
         // Comment any operation call out, to run the others on their own.
-        //this.inOrder();
+        this.inOrder();
         // this.reverseOrder();
+        System.out.println();
         // TODO Add code for homework exercise 3 (d) here
         //nicht mit Vorschlag von IntelliJ ersetzen! Ist Aufgabenstellung
-        Comparator<Integer> descendingOrder = (a , b) -> b.compareTo(a);
+        /*Comparator<Integer> descendingOrder = (a, b) -> b.compareTo(a);
         Comparator<Integer> ascendingOrder = (a, b) -> a.compareTo(b);
-        this.sort(descendingOrder);
+        this.sort(descendingOrder);*/
     }
-
-
 
 	/*@
 	  @ requires paule != null;
@@ -44,7 +43,6 @@ public class MemoryHamsterGame extends SimpleHamsterGame {
 	  @ ensures paule.mouthEmpty();
 	  @ ensures new order of grains on the field;
 	*/
-
     /**
      * Paule picks all grains and puts them down in the exact order as he has picked them up.
      * <p>
@@ -52,15 +50,14 @@ public class MemoryHamsterGame extends SimpleHamsterGame {
      * turn and walks back to his starting position, leaving the grains on each field in the exact order he has picked them up.
      */
     private void reverseOrder() {
-        assert !(paule == null);
         final Queue<Integer> reversedOrderList = new LinkedList<>(pickLineAndCount());
         turnAround(paule);
         while (!reversedOrderList.isEmpty()) {
             if (reversedOrderList.size() > 1) {
-                putMultipleGrains(paule,reversedOrderList.remove());
+                putMultipleGrains(paule, reversedOrderList.remove());
                 paule.move();
             } else {
-                putMultipleGrains(paule,reversedOrderList.remove());
+                putMultipleGrains(paule, reversedOrderList.remove());
             }
         }
         turnAround(paule);
@@ -72,7 +69,6 @@ public class MemoryHamsterGame extends SimpleHamsterGame {
 	  @ ensures order of grains per tile hasn´t changed;
 	  @ ensures paule.mouthIsEmpty();
 	@*/
-
     /**
      * Makes paule walk a line and pick up all grains then makes him return and leave the grains exactly like he has picked them up.
      * <p>
@@ -87,23 +83,28 @@ public class MemoryHamsterGame extends SimpleHamsterGame {
         paule.move();
         while (stack.contains(999)) {
             if (!reachedWall) {
-                reachedWall = walkForward(stack, reachedWall);
+                reachedWall = walkForward(stack);
             } else {
-                if (walkBack(stack))
-                    return;
+                if (walkBack(stack)) return;
             }
         }
         turnAround(paule);
     }
 
-
+    /*@
+      @ requires paule != null
+      @ ensures \new GrainCountOnField == 0;
+      @ ensures \new(GrainsInPaulesMouth) == \old(GrainsInPaulesMouth) + \old(GrainCountOnField)
+     */
     /**
      * Gets the amount of grains on a field and picks all the grains up
      *
-     * @return returns
+     * @return returns the number of grains that were on the field
      */
+    //TODO vllt. nochmal überlegen, wegen Abfragen- und Kommandotrennung, sonst maybe Stilfehler
     private int pickFieldAndCount(Hamster hamster) {
-        int grainCount = 0;
+        assert paule != null;
+        grainCount = 0;
         while (hamster.grainAvailable()) {
             hamster.pickGrain();
             grainCount++;
@@ -112,69 +113,104 @@ public class MemoryHamsterGame extends SimpleHamsterGame {
     }
 
 
-
-
-    // TODO implement homework exercise 3 (b)
 	/*@
 	  @ requires paule !== null;
 	  @ requires paule.frontIsClear();
 	  @ ensures new order of grains on the field, sorted numerically;
-	  @ ensures paule.mouthIsEmpty();
 	@*/
     /**
      * Makes paule run in a line and back, putting the grains down from the smallest number of grains per tile to the highest;
      * <p>
-     * paule runs a line and picks up grains until he reaches a wall. Once the wall is reached, he takes a 180° turn and moves.
+     * paule runs a line and picks up grains until he reaches a wall. Once the wall is reached, he does a 180° turn and moves.
      * He then proceeds to put down grains on each tile, sorting them by putting them down from the smallest amount to the highest amount of grains
      * per tile.
      */
     private void sort() {
         assert !(paule == null);
+
         SortedSet<Integer> sortedSet = new TreeSet<>(pickLineAndCount());
         turnAround(paule);
+
         for (final Integer step : sortedSet) {
-            paule.move();
-            putMultipleGrains(paule,step);
-        }
-        turnAround(paule);
-    }
-
-
-    // TODO Add Operation for homework exercise 3 (c) here
-
-    private void sort(Comparator<Integer> comparator){
-        assert paule != null;
-        SortedSet<Integer> sortedSet = new TreeSet<>(comparator);
-        sortedSet.addAll(pickLineAndCount());
-        turnAround(paule);
-        for(final Integer step : sortedSet){
             paule.move();
             putMultipleGrains(paule, step);
         }
         turnAround(paule);
     }
 
-    // TODO Add required helper operations here
 
-    private boolean walkForward(Stack<Integer> stack, boolean reachedWall) {
-        int amountOfGrains = pickFieldAndCount(paule);
-        stack.push(amountOfGrains);
-        if (paule.frontIsClear())
+    /*@
+      @ requires paule != null
+      @ ensures new order of grains is grains sorted numerically by the comparator
+      @ ensures
+     */
+    /**
+     * Makes paule walk a straight line picking up all the grains in the line, then turn around and walk back while
+     * putting all the grains he picked up back down, in sorted by whichever Comparator was chosen.
+     *
+     * @param comparator the comparator which the TreeSet is sorted by
+     */
+    private void sort(Comparator<Integer> comparator) {
+        assert paule != null;
+
+        SortedSet<Integer> sortedSet = new TreeSet<>(comparator);
+        sortedSet.addAll(pickLineAndCount());
+
+        turnAround(paule);
+        for (final Integer step : sortedSet) {
             paule.move();
-        else {
-            turnAround(paule);
-            reachedWall = true;
+            putMultipleGrains(paule, step);
         }
-        return reachedWall;
+        turnAround(paule);
     }
 
-    private boolean walkBack(Stack<Integer> stack) {
-        int currentStackValue = stack.pop();
-        if (currentStackValue == 999) {
+    /*@
+      @ requires paule != null;
+      @ stack != null;
+      @ ensures \new(stack.size()) = \old(stack.size()) + 1;
+      @ ensures stack.get(0) = grainCount;
+     */
+    /**
+     * Helper operation for inOrder.
+     * Picks up all the grains on a field and adds the amount of grains picked up to a stack, if paule's front is clear
+     * moves, if not turns around
+     *
+     * @param stack the stack the amount of grains is added to
+     * @return reachedWall true if paule has reached a wall, false if not
+     */
+    private boolean walkForward(Stack<Integer> stack) {
+        assert stack != null;
+
+        grainCount = pickFieldAndCount(paule);
+        stack.push(grainCount);
+        if (paule.frontIsClear()) paule.move();
+        else {
             turnAround(paule);
             return true;
         }
-        putMultipleGrains(paule, currentStackValue);
+        return false;
+    }
+
+    /*@
+      @ requires paule != null;
+      @ requires stack != null;
+      @ requires !stack.isEmpty();
+      @ ensures \new(stack.size()) = \old(stack.size()) - 1;
+      @ ensures \new(grainCountOnFiled)
+     */
+    /**
+     * Pops the first element of an integer stack and puts as many grains on the field
+     *
+     * @param stack the stack
+     * @return true if the current first value of the stack is 999, false if otherwise
+     */
+    private boolean walkBack(Stack<Integer> stack) {
+        grainCount = stack.pop();
+        if (grainCount == 999) {
+            turnAround(paule);
+            return true;
+        }
+        putMultipleGrains(paule, grainCount);
         paule.move();
         return false;
     }
@@ -204,7 +240,6 @@ public class MemoryHamsterGame extends SimpleHamsterGame {
 	  @ requires paule!== null;
 	  @ ensures paule.getDirection.equals(/old paule.getDirection) = false;
 	 */
-
     /**
      * Makes paule turn around 180°.
      */
@@ -221,13 +256,11 @@ public class MemoryHamsterGame extends SimpleHamsterGame {
 	  @ ensures !grainCountList.isEmpty();
 	  @ ensures grainCount is added to grainCountList;
 	@*/
-
     /**
      * Makes paule move a line and pick up every grain and add the number of grains per field to the linkedList grainCountList;
      *
      * @ return grainCountList LinkedList that has saved the number of grains picked up per field;
      */
-
     private LinkedList<Integer> pickLineAndCount() {
         assert (!(paule == null));
         final LinkedList<Integer> grainCountList = new LinkedList<>();
